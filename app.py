78c33a1c59a6bda5afde982226597dca621fe582
baigala <baigalma@gmail>
2020-08-04 16:43:00 +0200

    first ver

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, jsonify
+import joblib
+from flask_sqlalchemy import SQLAlchemy
+
+import os
+
+app = Flask(__name__)
+
+app.config.from_object(os.environ["APP_SETTINGS"])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db=SQLAlchemy(app)
+
+from models import Result
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        try:
+            regressor = joblib.load("linear_regression_model.pkl")
+            # Get years of experience from input form index.html
+            data = dict(request.form.items())
+            year_of_experience = float(data['YearsExperience'])
+            prediction = regressor.predict([[year_of_experience]])
+        except ValueError:
+            return jsonify("Please enter a number.")
+        result = Result(
+            YearsExperience=float(year_of_experience),
+            Prediction=float(prediction)
+        )
+        db.session.add(result)
+        db.session.commit()
+        return render_template('predicted.html', prediction=float(prediction))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,41 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SQLALCHEMY_DATABASE_URI = os.environ["DBHOST"]
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+        dbuser=os.environ['DBUSER'],
+        dbpass=os.environ['DBPASS'],
+        dbhost=os.environ['DBHOST'],
+        dbname=os.environ['DBNAME']
+    )
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+        dbuser=os.environ['DBUSER'],
+        dbpass=os.environ['DBPASS'],
+        dbhost=os.environ['DBHOST'],
+        dbname=os.environ['DBNAME']
+    )
+
+
+class TestingConfig(Config):
+    TESTING = True
